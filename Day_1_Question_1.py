39f3231a7827856105668d71913d18f105c92ea8
Tanvi33766 <tanviavitankar@gmail.com>
2024-06-21 22:10:37 +0530

    Day_1_Question_1.py

--- Day_1_Question_1.py
+++ Day_1_Question_1.py
@@ -1,1 +1,7 @@
-
+def count_candy_segments(candy_bar, birth_month, birth_day):
+  count = 0
+  for i in range(len(candy_bar) - birth_month + 1):  
+    segment_sum = sum(candy_bar[i:i + birth_month]) 
+    if segment_sum == birth_day:
+      count += 1
+  return count
